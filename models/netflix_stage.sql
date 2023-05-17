b60cdf3e63b33ccb3f0afc4f454beb564c0c58f6
Raji <nraji.sap@gmail.com>
2023-05-17 15:21:37 -0500

    model across schemas

--- models/netflix_stage.sql
+++ models/netflix_stage.sql
@@ -1,5 +1,13 @@
+{{
+  config(
+    schema='AIRBNB',
+    materialized='table'
+  )
+}}
+
 with netflix_stage as (
 select * from {{ source('s3','netflix_titles' )}}
 )
 select * from netflix_stage
 where type = 'Movie'
+
